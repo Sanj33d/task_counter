27203c16f495044ef2f88a977126fe1b61b9757d
Sanj33d <sanjeedsajeen8@gmail.com>
2025-03-02 06:23:48 +0600

    counter incremener, decrementer done

--- script/index.js
+++ script/index.js
@@ -1,17 +1,4 @@
-// document.getElementById("btn-amar1")
-// .addEventListener("click", function(){
-//     const val = getInnerTextById("task-assigned")
-
-//     const result = val - 1
-//     if (result < 0) {
-//         alert("You don't have any task assigned")
-//     }
-//     else {
-//         document.getElementById("task-assigned").innerText = result
-
-//     }
-// })
-
+// task assigned decrementing
 let arr = ["btn-amar1", "btn-amar2", "btn-amar3", "btn-amar4", "btn-amar5", "btn-amar6"]
 
 for (let x of arr) {
@@ -26,6 +13,25 @@ for (let x of arr) {
     else {
         document.getElementById("task-assigned").innerText = result
         document.getElementById(x).style.backgroundColor = "#9BA8F8"
+        
+        // task-completed incrementing
+        let taskCompleted = document.getElementById("task-completed").innerText
+        let newTaskCompleted = parseInt(taskCompleted) + 1
+        document.getElementById("task-completed").innerText = newTaskCompleted
+
+        // history log incrementing
+        let parent_elem = document.getElementById("history-container")
+
+        let new_elem = document.createElement("p")
+        new_elem.innerText = `
+        You have Complete The Task Add Dark Mode at`
+
+        parent_elem.appendChild(new_elem)
+
+
+
+
     }
 })
 }
+// 
